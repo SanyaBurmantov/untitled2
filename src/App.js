324670f119ae30f785faadfbbae851718f469935
SanyaBurmantov <Sanyaburmantov@gmail.com>
2022-07-28 18:37:41 +0300

    hehe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
+import React from 'react';
 import {Component} from "react";
 import Main from './components/MainComponent';
+import {BrowserRouter} from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
+            <BrowserRouter>
             <div className="App">
                 <Main />
             </div>
+            </BrowserRouter>
         );
     }
 }
